0b5483a66e1566f950e58cc40a3610c7924414d4
Gabriel Arteaga <gabrielarteaga71@gmail.com>
2017-06-26 02:47:56 -0700

    remade site with bulma

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,2 @@
 $(document).ready(function() {
-    // var sites = ['urbane', 'gomez', 'joie', 'slotog'];
-    // var count = sites.length;
-
-    // if ($(window).width() > 767) {
-    //     $('.work-container').each(function() {
-    //         $(this).hover(function() {
-    //             for (var ndx = 0; ndx < sites.length; ndx++) {
-    //                 if ($(this).hasClass(sites[ndx])) {
-    //                     expandPreview(sites[ndx]);
-    //                 }
-    //             }
-    //         });
-    //     });
-    // }
-
-    // function expandPreview(site) {
-    //     $('.work-expand.' + site).toggle();
-    // }
-
-    $(document).on('click', 'nav a', function(event){
-        event.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top
-        }, 500);
-    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var config = {
 	jsConcatFiles: [
 		'./app/js/main.js'
-	], 
+	],
 	buildFilesFoldersRemove:[
-		'build/sass/', 
+		'build/sass/',
 		'build/js/!(*.min.js)',
 		'build/bower.json',
 		'build/bower_components/',
@@ -20,8 +20,7 @@ var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
-    mainBowerFiles = require('gulp-main-bower-files'),
-    gulpFilter = require('gulp-filter'),
+   gulpFilter = require('gulp-filter'),
 	del = require('del');
 
 function errorlog(err){
@@ -30,95 +29,76 @@ function errorlog(err){
 }
 
 gulp.task('scripts', function() {
-  return gulp.src(config.jsConcatFiles)
-	.pipe(sourcemaps.init())
-		.pipe(concat('temp.js'))
-		.pipe(uglify())
-		.on('error', errorlog)
-		.pipe(rename('main.min.js'))		
-    .pipe(sourcemaps.write('../maps'))
-    .pipe(gulp.dest('./app/js/'))
-	.pipe(reload({stream:true}));
+   return gulp.src(config.jsConcatFiles)
+      .pipe(sourcemaps.init())
+      .pipe(concat('temp.js'))
+      .pipe(uglify())
+      .on('error', errorlog)
+      .pipe(rename('main.min.js'))
+      .pipe(sourcemaps.write('../maps'))
+      .pipe(gulp.dest('./app/js/'))
+      .pipe(reload({stream:true}));
 });
 
 
 gulp.task('styles', function() {
-	gulp.src('app/sass/style.scss')
-		.pipe(sourcemaps.init())
-			.pipe(sass({outputStyle: 'compressed'}))
-			.on('error', errorlog)
-			.pipe(autoprefixer({
-	            browsers: ['last 3 versions'],
-	            cascade: false
-	        }))	
-		.pipe(sourcemaps.write('../maps'))
-		.pipe(gulp.dest('app/css'))
-        .pipe(reload({stream:true}));
-});
-
-gulp.task('main-bower-files', function() {
-    var filterJS = gulpFilter('**/*.js', { restore: true });
-    return gulp.src('app/bower.json')
-        .pipe(mainBowerFiles({
-            overrides: {
-                bootstrap: {
-                    main: [
-                        './dist/js/bootstrap.js',
-                    ]
-                }
-            }
-        }))
-        .pipe(filterJS)
-        .pipe(concat('vendor.min.js'))
-        .pipe(uglify())
-        .pipe(filterJS.restore)
-        .pipe(gulp.dest('./app/js'));
+   gulp.src('app/sass/style.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({outputStyle: 'compressed'}))
+      .on('error', errorlog)
+      .pipe(autoprefixer({
+         browsers: ['last 3 versions'],
+         cascade: false
+      }))
+      .pipe(sourcemaps.write('../maps'))
+      .pipe(gulp.dest('app/css'))
+      .pipe(reload({stream:true}));
 });
 
 gulp.task('html', function() {
-	gulp.src('app/**/*.html')
-	.pipe(reload({stream:true}));
+   gulp.src('app/**/*.html')
+      .pipe(reload({stream:true}));
 });
 
 gulp.task('build:cleanfolder', function(cb) {
-	del([
-		'build/**'
-	], cb);
+   del([
+      'build/**'
+   ], cb);
 });
 
 gulp.task('build:copy', ['build:cleanfolder'], function() {
-	return gulp.src('app/**/*/')
-	.pipe(gulp.dest('build/'));
+   return gulp.src('app/**/*/')
+      .pipe(gulp.dest('build/'));
 });
 
 gulp.task('build:remove', ['build:copy'], function (cb) {
-	del(config.buildFilesFoldersRemove, cb);
+   del(config.buildFilesFoldersRemove, cb);
 });
 
 gulp.task('build', ['build:copy', 'build:remove']);
 
 gulp.task('browser-sync', function() {
-	browserSync({
-		server:{
-			baseDir: "./app/"
-		}
-	});
+   browserSync({
+      server:{
+         baseDir: "./app/"
+      }
+   });
 });
 
 
 // task to run build server for testing final app
 gulp.task('build:serve', function() {
-	browserSync({
-		server:{
-			baseDir: "./build/"
-		}
-	});
+   browserSync({
+      server:{
+         baseDir: "./build/"
+      }
+   });
 });
 
 gulp.task ('watch', function(){
-	gulp.watch('./app/js/**/*.js', ['scripts']);
-	gulp.watch('./app/sass/**/*.scss', ['styles']);
-	gulp.watch('./app/**/*.html', ['html']);
+   gulp.watch('./app/js/**/*.js', ['scripts']);
+   gulp.watch('./app/sass/**/*.scss', ['styles']);
+   gulp.watch('./app/**/*.html', ['html']);
 });
 
-gulp.task('default', ['scripts', 'styles', 'main-bower-files', 'html', 'browser-sync', 'watch']);
+gulp.task('default', ['scripts', 'styles', 'html', 'browser-sync', 'watch']);
